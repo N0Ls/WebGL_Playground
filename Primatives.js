bd72182e881b733bb7e14a95ce491400a72b4970
Landrodie <nilsland31@gmail.com>
2020-09-08 23:33:05 +0200

    Putting camera to use

--- Primatives.js
+++ Primatives.js
@@ -1,7 +1,8 @@
 var Primatives = {};
 
 Primatives.GridAxis = class{
-	static createMesh(gl){
+	static createModal(gl, inclAxis){ return new Modal(Primatives.GridAxis.createMesh(gl)); }
+	static createMesh(gl, inclAxis){
 		//Dynamiclly create a grid
 		var verts = [],
 			size = 1.8,			// W/H of the outer box of the grid, from origin we can only go 1 unit in each direction, so from left to right is 2 units max
@@ -10,30 +11,66 @@ Primatives.GridAxis = class{
 			half = size / 2;	// From origin the starting position is half the size.
 
 				var p;	//Temp variable for position value.
+		
 		for(var i=0; i <= div; i++){
 			//Vertical line
 			p = -half + (i * step);
 			verts.push(p);		//x1
-			verts.push(half);	//y1
-			verts.push(0);		//z1
+			verts.push(0);		//y1 verts.push(half);
+			verts.push(half);	//z1 verts.push(0);
 			verts.push(0);		//c2
 
 			verts.push(p);		//x2
-			verts.push(-half);	//y2
-			verts.push(0);		//z2
-			verts.push(1);		//c2
+			verts.push(0);		//y2 verts.push(-half);
+			verts.push(-half);	//z2 verts.push(0);	
+			verts.push(0);		//c2 verts.push(1);
 
 			//Horizontal line
 			p = half - (i * step);
 			verts.push(-half);	//x1
-			verts.push(p);		//y1
-			verts.push(0);		//z1
+			verts.push(0);		//y1 verts.push(p);
+			verts.push(p);		//z1 verts.push(0);
 			verts.push(0);		//c1
 
 			verts.push(half);	//x2
-			verts.push(p);		//y2
+			verts.push(0);		//y2 verts.push(p);
+			verts.push(p);		//z2 verts.push(0);
+			verts.push(0);		//c2 verts.push(1);
+		}
+
+		if(inclAxis){
+			//x axis
+			verts.push(-1.1);	//x1
+			verts.push(0);		//y1
+			verts.push(0);		//z1
+			verts.push(1);		//c2
+
+			verts.push(1.1);	//x2
+			verts.push(0);		//y2
 			verts.push(0);		//z2
 			verts.push(1);		//c2
+
+			//y axis
+			verts.push(0);//x1
+			verts.push(-1.1);	//y1
+			verts.push(0);		//z1
+			verts.push(2);		//c2
+
+			verts.push(0);		//x2
+			verts.push(1.1);	//y2
+			verts.push(0);		//z2
+			verts.push(2);		//c2
+
+			//z axis
+			verts.push(0);		//x1
+			verts.push(0);		//y1
+			verts.push(-1.1);	//z1
+			verts.push(3);		//c2
+
+			verts.push(0);		//x2
+			verts.push(0);		//y2
+			verts.push(1.1);	//z2
+			verts.push(3);		//c2
 		}
 
 		var attrColorLoc = 4,
